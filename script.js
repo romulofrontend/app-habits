0ea8b4f353feeaf961f0c8626f0fbc101c976325
Romulo Rosa <romulo.frontend@gmail.com>
2023-01-20 15:32:05 -0300

    js var pra form e construtor

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+//Criar variavel pra armazenar o form
+const form = document.querySelector("#form-habits")
+//Usando construtor pra criar variavel inicializar biblioteca
+const nlwSetup = new NLWSetup(form)
