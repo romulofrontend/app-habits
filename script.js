6f555ea1ba64d74f63bafa0e70ff6171a8b2aa06
Romulo Rosa <romulo.frontend@gmail.com>
2023-01-20 19:53:02 -0300

    js inserir evento no botao

--- script.js
+++ script.js
@@ -3,24 +3,48 @@
 
 //Criar variavel pra armazenar o form
 const form = document.querySelector("#form-habits")
+
 //Usando construtor pra criar variavel inicializar biblioteca
 const nlwSetup = new NLWSetup(form)
+
+//Criar variavel pra armazenar o botão
+const buttonRegistrar = document.querySelector("header button")
+//Escutando esse botão - Ao clicar dispara a função add() pra incluir o dia de hoje
+buttonRegistrar.addEventListener("click", add)
 
 //Criar variável pra armazenar objeto data
 //Objeto contém arrays referentes a cada hábito
 //De acordo com a doc da biblioteca, declarar as datas que aparecem checked (formato mm-dd)
 const data = {
-  run: ["01-01","01-07","01-08"],
+  run: ["01-01", "01-07", "01-08"],
 
-  water: ["01-01","01-02","01-03","01-04","01-05","01-06","01-07","01-08"],
+  water: [
+    "01-01",
+    "01-02",
+    "01-03",
+    "01-04",
+    "01-05",
+    "01-06",
+    "01-07",
+    "01-08",
+  ],
 
-  gym: ["01-02","01-04","01-06"],
+  gym: ["01-02", "01-04", "01-06"],
 
-  sleep: ["01-01","01-02","01-03","01-04","01-05","01-08"],
+  sleep: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-08"],
 
-  pets: ["01-01","01-02","01-03","01-04","01-05","01-06","01-07","01-08"],
+  pets: [
+    "01-01",
+    "01-02",
+    "01-03",
+    "01-04",
+    "01-05",
+    "01-06",
+    "01-07",
+    "01-08",
+  ],
 
-  food: ["01-02","01-03","01-04","01-05","01-06","01-07"],
+  food: ["01-02", "01-03", "01-04", "01-05", "01-06", "01-07"],
 }
 
 //com o objeto declarado acima, inserimos esses dados no form atraves da função .setData(data)
